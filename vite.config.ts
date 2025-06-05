ef90546a9f8a809e7c9d769cdf242f05c36b0643
Edakun <kunitetsu52@gmail.com>
2025-06-05 22:10:29 +0900

    ベースパスを相対パスに変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Vite設定
 export default defineConfig({
   plugins: [react()],
-  // アセットのベースパスを設定
-  base: '/maruyo-quiz/',
-  // ビルド設定
+  base: './', // 相対パスに変更
   build: {
-    // ビルド関連の設定をここに追加可能
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true
